26d9abd2cf97023a496f8bb8d0e8e8a47da86439
asadullahjamalov <asadullahnj@code.edu.az>
2021-07-21 17:16:43 +0400

    initial commit

---
+++ src/main/java/com/example/buytourwebproject/BuyTourWebProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.buytourwebproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyTourWebProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuyTourWebProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/buytourwebproject/BuyTourWebProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.buytourwebproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuyTourWebProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
